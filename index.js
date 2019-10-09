527f704366fc757d84f6536ab822093186b94524
Nemi Shah <nemishah1212@gmail.com>
2019-10-09 12:40:31 +0530

    Adds es5 compatibility

--- index.js
+++ index.js
@@ -1,20 +1,53 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 /**
  * @constant
  * @type {string}
  * @default
  * @description All the locks taken by this package will have this as prefix
 */
-const LOCK_STORAGE_KEY = 'browser-tabs-lock-key';
-
+var LOCK_STORAGE_KEY = 'browser-tabs-lock-key';
 /**
  * @function delay
  * @param {number} milliseconds - How long the delay should be in terms of milliseconds
- * @returns {Promise<void>} 
+ * @returns {Promise<void>}
  */
 function delay(milliseconds) {
-    return new Promise(resolve => setTimeout(resolve, milliseconds));
+    return new Promise(function (resolve) { return setTimeout(resolve, milliseconds); });
 }
-
 /**
  * @function generateRandomString
  * @params {number} length - How long the random string should be
@@ -22,27 +55,24 @@ function delay(milliseconds) {
  * @description returns random string whose length is equal to the length passed as parameter
  */
 function generateRandomString(length) {
-    const CHARS = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz';
-    let randomstring = '';
-    for (let i = 0; i < length; i++) {
-        const INDEX = Math.floor(Math.random() * CHARS.length);
+    var CHARS = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz';
+    var randomstring = '';
+    for (var i = 0; i < length; i++) {
+        var INDEX = Math.floor(Math.random() * CHARS.length);
         randomstring += CHARS[INDEX];
     }
     return randomstring;
 }
-
 /**
  * @function getLockId
  * @returns {string}
  * @description Generates an id which will be unique for the browser tab
  */
 function getLockId() {
-    return Date.now().toString() + generateRandomString(15)
+    return Date.now().toString() + generateRandomString(15);
 }
-
-class Lock {
-
-    constructor() {
+var Lock = /** @class */ (function () {
+    function Lock() {
         this.id = getLockId();
         this.acquireLock = this.acquireLock.bind(this);
         this.releaseLock = this.releaseLock.bind(this);
@@ -52,7 +82,6 @@ class Lock {
             Lock.waiters = [];
         }
     }
-
     /**
      * @async
      * @memberOf Lock
@@ -60,86 +89,120 @@ class Lock {
      * @param {string} lockKey - Key for which the lock is being acquired
      * @param {number} [timeout=5000] - Maximum time for which the function will wait to acquire the lock
      * @returns {Promise<boolean>}
-     * @description Will return true if lock is being acuired, else false.
+     * @description Will return true if lock is being acquired, else false.
      *              Also the lock can be acquired for maximum 10 secs
      */
-    async acquireLock(lockKey, timeout = 5000) {
-        let iat = Date.now() + generateRandomString(4);
-        const MAX_TIME = Date.now() + timeout;
-        const STORAGE_KEY = `${LOCK_STORAGE_KEY}-${lockKey}`;
-        const STORAGE = window.localStorage;
-        while (Date.now() < MAX_TIME) {
-            let lockObj = STORAGE.getItem(STORAGE_KEY);
-            if (lockObj === null) {
-                const TIMEOUT_KEY = `${this.id}-${lockKey}-${iat}`;
-                // there is a problem if setItem happens at the exact same time for 2 different processes.. so we add some random delay here.
-                await delay(Math.floor(Math.random() * 25));
-                STORAGE.setItem(STORAGE_KEY, JSON.stringify({
-                    id: this.id,
-                    iat,
-                    timeoutKey: TIMEOUT_KEY,
-                    timeAcquired: Date.now()
-                }));
-                await delay(30);    // this is to prevent race conditions. This time must be more than the time it takes for storage.setItem
-                let lockObjPostDelay = STORAGE.getItem(STORAGE_KEY);
-                if (lockObjPostDelay !== null) {
-                    lockObjPostDelay = JSON.parse(lockObjPostDelay);
-                    if (lockObjPostDelay.id === this.id && lockObjPostDelay.iat === iat) {
-                        return true;
-                    }
+    Lock.prototype.acquireLock = function (lockKey, timeout) {
+        if (timeout === void 0) { timeout = 5000; }
+        return __awaiter(this, void 0, void 0, function () {
+            var iat, MAX_TIME, STORAGE_KEY, STORAGE, lockObj, TIMEOUT_KEY, lockObjPostDelay;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        iat = Date.now() + generateRandomString(4);
+                        MAX_TIME = Date.now() + timeout;
+                        STORAGE_KEY = LOCK_STORAGE_KEY + "-" + lockKey;
+                        STORAGE = window.localStorage;
+                        _a.label = 1;
+                    case 1:
+                        if (!(Date.now() < MAX_TIME)) return [3 /*break*/, 7];
+                        lockObj = STORAGE.getItem(STORAGE_KEY);
+                        if (!(lockObj === null)) return [3 /*break*/, 4];
+                        TIMEOUT_KEY = this.id + "-" + lockKey + "-" + iat;
+                        // there is a problem if setItem happens at the exact same time for 2 different processes.. so we add some random delay here.
+                        return [4 /*yield*/, delay(Math.floor(Math.random() * 25))];
+                    case 2:
+                        // there is a problem if setItem happens at the exact same time for 2 different processes.. so we add some random delay here.
+                        _a.sent();
+                        STORAGE.setItem(STORAGE_KEY, JSON.stringify({
+                            id: this.id,
+                            iat: iat,
+                            timeoutKey: TIMEOUT_KEY,
+                            timeAcquired: Date.now()
+                        }));
+                        return [4 /*yield*/, delay(30)];
+                    case 3:
+                        _a.sent(); // this is to prevent race conditions. This time must be more than the time it takes for storage.setItem
+                        lockObjPostDelay = STORAGE.getItem(STORAGE_KEY);
+                        if (lockObjPostDelay !== null) {
+                            lockObjPostDelay = JSON.parse(lockObjPostDelay);
+                            if (lockObjPostDelay.id === this.id && lockObjPostDelay.iat === iat) {
+                                return [2 /*return*/, true];
+                            }
+                        }
+                        return [3 /*break*/, 6];
+                    case 4:
+                        lockCorrector();
+                        return [4 /*yield*/, this.waitForSomethingToChange(MAX_TIME)];
+                    case 5:
+                        _a.sent();
+                        _a.label = 6;
+                    case 6:
+                        iat = Date.now() + generateRandomString(4);
+                        return [3 /*break*/, 1];
+                    case 7: return [2 /*return*/, false];
                 }
-            } else {
-                lockCorrector();
-                await this.waitForSomethingToChange(MAX_TIME);
-            }
-            iat = Date.now() + generateRandomString(4);
-        }
-        return false;
-    }
-
-    async waitForSomethingToChange(MAX_TIME) {
-        await new Promise(resolve => {
-            let resolvedCalled = false;
-            let startedAt = Date.now();
-            const MIN_TIME_TO_WAIT = 50;    // ms
-            let removedListeners = false;
-            function stopWaiting() {
-                if (!removedListeners) {
-                    window.removeEventListener('storage', stopWaiting);
-                    Lock.removeFromWaiting(stopWaiting);
-                    clearTimeout(timeOutId);
-                    removedListeners = true;
-                }
-                if (!resolvedCalled) {
-                    resolvedCalled = true;
-                    let timeToWait = MIN_TIME_TO_WAIT - (Date.now() - startedAt);
-                    if (timeToWait > 0) {
-                        setTimeout(resolve, timeToWait);
-                    } else {
-                        resolve();
-                    }
-                }
-            }
-            window.addEventListener('storage', stopWaiting);
-            Lock.addToWaiting(stopWaiting);
-            let timeOutId = setTimeout(stopWaiting, Math.max(0, MAX_TIME - Date.now()));
+            });
         });
-    }
-
-    static addToWaiting(func) {
+    };
+    Lock.prototype.waitForSomethingToChange = function (MAX_TIME) {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, new Promise(function (resolve) {
+                            var resolvedCalled = false;
+                            var startedAt = Date.now();
+                            var MIN_TIME_TO_WAIT = 50; // ms
+                            var removedListeners = false;
+                            function stopWaiting() {
+                                if (!removedListeners) {
+                                    window.removeEventListener('storage', stopWaiting);
+                                    Lock.removeFromWaiting(stopWaiting);
+                                    clearTimeout(timeOutId);
+                                    removedListeners = true;
+                                }
+                                if (!resolvedCalled) {
+                                    resolvedCalled = true;
+                                    var timeToWait = MIN_TIME_TO_WAIT - (Date.now() - startedAt);
+                                    if (timeToWait > 0) {
+                                        setTimeout(resolve, timeToWait);
+                                    }
+                                    else {
+                                        resolve();
+                                    }
+                                }
+                            }
+                            window.addEventListener('storage', stopWaiting);
+                            Lock.addToWaiting(stopWaiting);
+                            var timeOutId = setTimeout(stopWaiting, Math.max(0, MAX_TIME - Date.now()));
+                        })];
+                    case 1:
+                        _a.sent();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    Lock.addToWaiting = function (func) {
         this.removeFromWaiting(func);
+        if (Lock.waiters === undefined) {
+            return;
+        }
         Lock.waiters.push(func);
-    }
-
-    static removeFromWaiting(func) {
-        Lock.waiters = Lock.waiters.filter(i => i !== func);
-    }
-
-    static notifyWaiters() {
-        let waiters = [...Lock.waiters];    // so that if Lock.waiters is changed it's ok.
-        waiters.forEach(i => i());
-    }
-
+    };
+    Lock.removeFromWaiting = function (func) {
+        if (Lock.waiters === undefined) {
+            return;
+        }
+        Lock.waiters = Lock.waiters.filter(function (i) { return i !== func; });
+    };
+    Lock.notifyWaiters = function () {
+        if (Lock.waiters === undefined) {
+            return;
+        }
+        var waiters = Lock.waiters.slice(); // so that if Lock.waiters is changed it's ok.
+        waiters.forEach(function (i) { return i(); });
+    };
     /**
      * @function releaseLock
      * @memberOf Lock
@@ -147,10 +210,9 @@ class Lock {
      * @returns {void}
      * @description Release a lock.
      */
-    releaseLock(lockKey) {
+    Lock.prototype.releaseLock = function (lockKey) {
         return this.releaseLock__private__(lockKey);
-    }
-
+    };
     /**
      * @function releaseLock
      * @memberOf Lock
@@ -159,10 +221,11 @@ class Lock {
      * @returns {void}
      * @description Release a lock.
      */
-    releaseLock__private__(lockKey, iat = null) {
-        const STORAGE = window.localStorage;
-        const STORAGE_KEY = `${LOCK_STORAGE_KEY}-${lockKey}`;
-        let lockObj = STORAGE.getItem(STORAGE_KEY);
+    Lock.prototype.releaseLock__private__ = function (lockKey, iat) {
+        if (iat === void 0) { iat = null; }
+        var STORAGE = window.localStorage;
+        var STORAGE_KEY = LOCK_STORAGE_KEY + "-" + lockKey;
+        var lockObj = STORAGE.getItem(STORAGE_KEY);
         if (lockObj === null) {
             return;
         }
@@ -171,9 +234,11 @@ class Lock {
             STORAGE.removeItem(STORAGE_KEY);
             Lock.notifyWaiters();
         }
-    }
-}
-
+    };
+    Lock.waiters = undefined;
+    return Lock;
+}());
+export default Lock;
 /**
  * @function lockCorrector
  * @returns {void}
@@ -181,14 +246,14 @@ class Lock {
  *              released, this function will release those locks
  */
 function lockCorrector() {
-    const MIN_ALLOWED_TIME = Date.now() - 10000;
-    const STORAGE = window.localStorage;
-    const KEYS = Object.keys(STORAGE);
-    let notifyWaiters = false;
-    for (let i = 0; i < KEYS.length; i++) {
-        const LOCK_KEY = KEYS[i];
+    var MIN_ALLOWED_TIME = Date.now() - 10000;
+    var STORAGE = window.localStorage;
+    var KEYS = Object.keys(STORAGE);
+    var notifyWaiters = false;
+    for (var i = 0; i < KEYS.length; i++) {
+        var LOCK_KEY = KEYS[i];
         if (LOCK_KEY.includes(LOCK_STORAGE_KEY)) {
-            let lockObj = STORAGE.getItem(LOCK_KEY);
+            var lockObj = STORAGE.getItem(LOCK_KEY);
             if (lockObj !== null) {
                 lockObj = JSON.parse(lockObj);
                 if (lockObj.timeAcquired < MIN_ALLOWED_TIME) {
@@ -202,5 +267,3 @@ function lockCorrector() {
         Lock.notifyWaiters();
     }
 }
-
-module.exports = Lock;
